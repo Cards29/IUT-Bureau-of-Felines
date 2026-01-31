1480a331bb4898ec78de61ca5e58097e241d931a
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-01-31 21:00:00 +0600

    Create User model with Google OAuth fields

---
+++ server/src/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  googleId: { type: String, unique: true, sparse: true },
+  email: { type: String, unique: true, sparse: true },
+  username: { type: String, required: true, unique: true, index: true },
+  displayName: { type: String },
+  avatarUrl: { type: String },
+}, { timestamps: true });
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
